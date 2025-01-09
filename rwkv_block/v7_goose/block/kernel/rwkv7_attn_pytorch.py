7e4fe789d8298a63c15303e3550aed89e65ec0fd
Eugene Cheah (picocreator) <eugeneqin@gmail.com>
2025-01-09 09:28:17 +0000

    semi optimized pure pytorch

--- rwkv_block/v7_goose/block/kernel/rwkv7_attn_pytorch.py
+++ rwkv_block/v7_goose/block/kernel/rwkv7_attn_pytorch.py
@@ -9,11 +9,72 @@ def rwkv7_attn_pytorch(
     BATCH_SIZE, SEQ_LEN, N_HEAD, HEAD_SIZE,
     xx, wkv_state_in
 ):
-    return rwkv7_attn_pytorch_ref(
-        r,w,k,v, kk,a,
-        BATCH_SIZE, SEQ_LEN, N_HEAD, HEAD_SIZE,
-        xx, wkv_state_in
-    )
+
+    ### Reference implement
+    # return rwkv7_attn_pytorch_ref(
+    #     r,w,k,v, kk,a,
+    #     BATCH_SIZE, SEQ_LEN, N_HEAD, HEAD_SIZE,
+    #     xx, wkv_state_in
+    # )
+    ###
+
+    # > per 9k chunk, per block, on a 4090 ...
+    # > with forward_with_reduce_compile on the timemix ...
+    #
+    # Somehow...
+    # The reference implement takes: 2281ms
+    # The chunked version takes:     141ms  (chunksize 256)
+
+    # Get the shape
+    B,T,HC = w.shape
+
+    # Compute the chunks
+    chunk_size = 256
+    chunk_count = SEQ_LEN // chunk_size
+    chunk_remainder = SEQ_LEN % chunk_size
+
+    # The wkv_state_out
+    wkv_state_out = wkv_state_in
+
+    # # List of tensor to build
+    # xlist = []
+
+    # Loop over the chunks
+    for i in range(chunk_count):
+        sta = i * chunk_size
+        end = sta + chunk_size
+
+        xx[:,sta:end], wkv_state_out = rwkv7_attn_pytorch_chunk_with_nocompile(
+        # xpart, wkv_state_out = rwkv7_attn_pytorch_chunk_with_nocompile(
+            r[:,sta:end],w[:,sta:end],k[:,sta:end],v[:,sta:end], 
+            kk[:,sta:end],a[:,sta:end],
+            BATCH_SIZE, N_HEAD, HEAD_SIZE,
+            xx[:,sta:end], wkv_state_out
+            # torch.zeros(B,chunk_size,HC, dtype=xx.dtype, device=xx.device), wkv_state_out
+        )
+        # xlist.append(xpart)
+
+    # Handle the remainder
+    if chunk_remainder > 0:
+        sta = chunk_count * chunk_size
+        end = sta + chunk_remainder
+
+        # xx[:,sta:end], wkv_state_out = rwkv7_attn_pytorch_chunk_with_nocompile(
+        xpart, wkv_state_out = rwkv7_attn_pytorch_chunk_with_nocompile(
+            r[:,sta:end],w[:,sta:end],k[:,sta:end],v[:,sta:end], 
+            kk[:,sta:end],a[:,sta:end],
+            BATCH_SIZE, N_HEAD, HEAD_SIZE,
+            # xx[:,sta:end], wkv_state_out,
+            torch.zeros(B,chunk_remainder,HC, dtype=xx.dtype, device=xx.device), wkv_state_out,
+            offset=0, chunk_size=chunk_remainder
+        )
+        # xlist.append(xpart)
+
+    # # Concatenate the list
+    # xx = torch_cat_no_compiler(xlist, dim=1)
+
+    # Return the output
+    return xx, wkv_state_out
 
 ####################################################################################################
 # Working reference copy, that has been validated to be "identical" to the reference implementation
@@ -79,6 +140,56 @@ def rwkv7_attn_pytorch_chunk(
     ######## pure pytorch method
     # See: https://github.com/BlinkDL/RWKV-LM/blob/d4c42b2cac10f8f3896ce153e2310dc763662b7a/RWKV-v7/rwkv_v7_demo_fast.py#L238
     ########
+    vk_state = wkv_state_in.float()
+    for i in range(chunk_size):
+        t = offset + i
+        r_, w_, k_, v_, kk_, a_ = r[:,t], w[:,t], k[:,t], v[:,t], kk[:,t], a[:,t]
+        vk = v_.view(BATCH_SIZE,N_HEAD,HEAD_SIZE,1) @ k_.view(BATCH_SIZE,N_HEAD,1,HEAD_SIZE)
+        ab = (-kk_).view(BATCH_SIZE,N_HEAD,HEAD_SIZE,1) @ (kk_*a_).view(BATCH_SIZE,N_HEAD,1,HEAD_SIZE)
+        vk_state = (vk_state * w_.view(BATCH_SIZE,N_HEAD,1,HEAD_SIZE).float() + vk_state @ ab.float() + vk.float())
+        xx[:,t] = (vk_state.to(dtype=xx.dtype) @ r_.view(BATCH_SIZE,N_HEAD,HEAD_SIZE,1)).view(BATCH_SIZE,N_HEAD*HEAD_SIZE)
+    wkv_state_out = vk_state.to(dtype=wkv_state_in.dtype)
+    return xx, wkv_state_out
+
+@torch.compile(mode="reduce-overhead", fullgraph=True)
+def rwkv7_attn_pytorch_chunk_with_reduce_compile(
+    r,w,k,v, kk,a, 
+    BATCH_SIZE, N_HEAD, HEAD_SIZE,
+    xx, wkv_state_in,
+    offset=0, chunk_size=16
+):
+    return rwkv7_attn_pytorch_chunk(
+        r,w,k,v, kk,a, 
+        BATCH_SIZE, N_HEAD, HEAD_SIZE,
+        xx, wkv_state_in,
+        offset, chunk_size
+    )
+
+@torch.compile(fullgraph=True)
+def rwkv7_attn_pytorch_chunk_with_fullgraph(
+    r,w,k,v, kk,a, 
+    BATCH_SIZE, N_HEAD, HEAD_SIZE,
+    xx, wkv_state_in,
+    offset=0, chunk_size=16
+):
+    return rwkv7_attn_pytorch_chunk(
+        r,w,k,v, kk,a, 
+        BATCH_SIZE, N_HEAD, HEAD_SIZE,
+        xx, wkv_state_in,
+        offset, chunk_size
+    )
+
+@torch.compiler.disable()
+def torch_cat_no_compiler(xlist, dim=1):
+    return torch.cat(xlist, dim=1)
+
+@torch.compiler.disable()
+def rwkv7_attn_pytorch_chunk_with_nocompile(
+    r,w,k,v, kk,a, 
+    BATCH_SIZE, N_HEAD, HEAD_SIZE,
+    xx, wkv_state_in,
+    offset=0, chunk_size=16
+):
     vk_state = wkv_state_in.float()
     for i in range(chunk_size):
         t = offset + i
