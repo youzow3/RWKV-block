80a4f119400aaee09f441f9b1f62647ced3c0857
Eugene Cheah (picocreator) <eugeneqin@gmail.com>
2025-01-15 16:37:03 +0000

    WIP cuda support

--- rwkv_block/v7_goose/block/kernel/rwkv7_attn_cuda.py
+++ rwkv_block/v7_goose/block/kernel/rwkv7_attn_cuda.py
@@ -1,4 +1,4 @@
-import torch, os
+import torch, os, time
 
 ####################################################################################################
 # Start of pytorch code
@@ -9,22 +9,37 @@ from .rwkv7_attn_pytorch import rwkv7_attn_pytorch_chunk
 def load_wkv_cuda_kernel(CHUNK_LEN = 16, HEAD_SIZE = 64):
     from torch.utils.cpp_extension import load
 
-    # load_name = f"wkv_wind_backstepping_C{HEAD_SIZE}_L{CHUNK_LEN}"
-    load_name = f"wkv_wind_backstepping"
+    # load_name = f"wind_backstepping_C{HEAD_SIZE}_L{CHUNK_LEN}"
+    load_name = f"wind_backstepping"
 
     # Check if the load_name is already loaded
     if load_name in torch.ops:
-        return torch.ops[load_name]
+        return torch.ops.wind_backstepping
     
     # Get the this script file path, to cmpute the cuda path
     this_file_path = os.path.dirname(os.path.abspath(__file__))
 
-    # Load the kernel
+    # Load the kernel, there is some wierd edge condition in compilation,
+    # that try catching.... and trying again.... sometimes work?
     flags = ['-res-usage', f'-D_C_={HEAD_SIZE}', f"-D_CHUNK_LEN_={CHUNK_LEN}", "--use_fast_math", "-O3", "-Xptxas -O3", "--extra-device-vectorization", "--forward-unknown-to-host-compiler"]
-    load(name=load_name, sources=[f'{this_file_path}/cuda/wkv7_cuda.cu', f'{this_file_path}/cuda/wkv7_op.cpp'], is_python_module=False, verbose=True, extra_cuda_cflags=flags)
+
+    try:
+        load(name=load_name, sources=[f'{this_file_path}/cuda/wkv7_cuda.cu', f'{this_file_path}/cuda/wkv7_op.cpp'], is_python_module=False, verbose=True, extra_cuda_cflags=flags)
+    except Exception as e:
+        print("[WARNING] Failed to load the kernel, trying again (sometimes the compiler has wierd race condition)...")
+        time.sleep(1) # Somehow this works, with minor compilation error, that passes on subsequent reruns
+        load(name=load_name, sources=[f'{this_file_path}/cuda/wkv7_cuda.cu', f'{this_file_path}/cuda/wkv7_op.cpp'], is_python_module=False, verbose=True, extra_cuda_cflags=flags)
 
     # Return the loaded kernel
-    return torch.ops[load_name]
+    return torch.ops.wind_backstepping
+
+@torch.compiler.disable()
+def wkv_cuda_forward(state, w,q,k,v,z,b, y,s,sa):
+    torch.ops.wind_backstepping.forward(state, w,q,k,v,z,b, y,s,sa)
+
+@torch.compiler.disable()
+def wkv_cuda_backward(state, w,q,k,v,z,b, dy,s,sa, dw,dq,dk,dv,dz,db):
+    torch.ops.wind_backstepping.backward(state, w,q,k,v,z,b, dy,s,sa, dw,dq,dk,dv,dz,db)
 
 class CudaWindBackstepping(torch.autograd.Function):
     @staticmethod
@@ -37,7 +52,7 @@ class CudaWindBackstepping(torch.autograd.Function):
         y = torch.empty_like(v)
         s = torch.empty(B,H,T//CHUNK_LEN,C,C, dtype=torch.float32,device=w.device)
         sa = torch.empty(B,T,H,C, dtype=torch.float32,device=w.device)
-        torch.ops.wkv_wind_backstepping.forward(state, w,q,k,v,z,b, y,s,sa)
+        wkv_cuda_forward(state, w,q,k,v,z,b, y,s,sa)
         ctx.save_for_backward(w,q,k,v,z,b,s,sa)
         return y, state
     @staticmethod
@@ -46,7 +61,7 @@ class CudaWindBackstepping(torch.autograd.Function):
         assert all(i.is_contiguous() for i in [dy])
         state,w,q,k,v,z,b,s,sa = ctx.saved_tensors
         dw,dq,dk,dv,dz,db = [torch.empty_like(x) for x in [w,q,k,v,z,b]]
-        torch.ops.wkv_wind_backstepping.backward(state, w,q,k,v,z,b, dy,s,sa, dw,dq,dk,dv,dz,db)
+        wkv_cuda_backward(state, w,q,k,v,z,b, dy,s,sa, dw,dq,dk,dv,dz,db)
         return dw,dq,dk,dv,dz,db
 
 def rwkv7_attn_cuda(r,w,k,v, kk,kk_a, HEAD_SIZE=64, s0=None):
