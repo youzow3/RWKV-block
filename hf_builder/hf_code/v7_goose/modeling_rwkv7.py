d85915a004d838e763cb628b6c2b6764ffc319ea
Eugene Cheah (picocreator) <eugeneqin@gmail.com>
2025-02-02 19:57:18 +0000

    WIP HF compatible model

--- hf_builder/hf_code/v7_goose/modeling_rwkv7.py
+++ hf_builder/hf_code/v7_goose/modeling_rwkv7.py
@@ -11,16 +11,16 @@ from transformers.utils import (
     logging,
 )
 from transformers.generation import GenerationMixin
-from transformers.modeling_outputs import ( ModelOutput,
-                                            BaseModelOutputWithPast,
-                                            CausalLMOutputWithPast)
+from transformers.modeling_outputs import ModelOutput
 
 import torch
 from torch import nn
+from torch.nn import CrossEntropyLoss
+import torch.nn.functional as F
 
 import warnings
 from dataclasses import dataclass
-from typing import List, Dict, Optional, Tuple, Union
+from typing import List, Dict, Optional, Tuple, Union, Any
 
 from .configuration_rwkv7 import RWKV7Config
 from .rwkv_block.v7_goose.model.rwkv7_goose_model import RWKV7GooseModel
@@ -178,7 +178,7 @@ RWKV7_INPUTS_DOCSTRING = r"""
 """
 
 @add_start_docstrings(
-    "The bare RWKV7 Model transformer outputting raw hidden-states without any specific head on top.",
+    "The bare RWKV7 Model transformer outputting raw hidden-states without activating the head (variable is still declared)",
     RWKV7_START_DOCSTRING,
 )
 class RWKV7Model(RWKV7PreTrainedModel):
@@ -195,10 +195,14 @@ class RWKV7Model(RWKV7PreTrainedModel):
     def set_output_embeddings(self, new_embeddings):
         self.lm_head = new_embeddings
 
+    @add_start_docstrings_to_model_forward(RWKV7_INPUTS_DOCSTRING)
+    @add_code_sample_docstrings(
+        output_type=RWKV7Output,
+    )
     def forward(
         self,
         input_ids: Optional[torch.LongTensor] = None,
-        attention_mask: Optional[torch.LongTensor] = None,  # noqa
+        attention_mask: Optional[torch.LongTensor] = None,  # not in use
         inputs_embeds: Optional[torch.FloatTensor] = None,
         rwkv_state: Optional[list[tuple[torch.Tensor,torch.Tensor,torch.Tensor]]] = None,
         use_cache: Optional[bool] = None,
@@ -206,7 +210,7 @@ class RWKV7Model(RWKV7PreTrainedModel):
         output_hidden_states: Optional[bool] = None,
         return_dict: Optional[bool] = None,
         **kwargs
-    ) -> Union[Tuple, BaseModelOutputWithPast]:
+    ) -> Union[Tuple, RWKV7Output]:
         
         output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
         output_hidden_states = output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
@@ -217,13 +221,19 @@ class RWKV7Model(RWKV7PreTrainedModel):
             warnings.warning_once("`RWKV7Model` does not `output_attentions` now, setting it to `False`.")
             output_attentions = False
         
-        # if output_hidden_states:
-        #     warnings.warning_once("`RWKV7Model` does not `output_hidden_states` now, setting it to `False`.")
-        #     output_hidden_states = False
-
         if self.gradient_checkpointing and self.training and use_cache:
             warnings.warning_once("`use_cache=True` is incompatible with gradient checkpointing. Setting `use_cache=False`...")
             use_cache = False
+ 
+        if self.gradient_checkpointing and self.training and use_cache:
+            warnings.warning_once("`use_cache=True` is incompatible with gradient checkpointing. Setting `use_cache=False`...")
+            use_cache = False
+
+        if output_hidden_states:
+            warnings.warning_once("`RWKV7Model` does not `output_hidden_states` now, setting it to `False`.")
+            output_hidden_states = False
+
+        # ---
 
         # Compute the input embeddings
         if input_ids is not None and inputs_embeds is not None:
@@ -234,21 +244,20 @@ class RWKV7Model(RWKV7PreTrainedModel):
             inputs_embeds = self.emb(input_ids.to(self.emb.weight.device))
         x_hidden_state = inputs_embeds
 
-        if use_cache and rwkv_state is None:
+        # Initialize the rwkv_state / prv_stateList
+        if rwkv_state is None or use_cache == False:
             rwkv_state = self.get_init_state(batch_size=x_hidden_state.shape[0])
         prv_stateList = rwkv_state
-        ret_stateList = self.get_init_state(batch_size=x_hidden_state.shape[0], skip_init_state=True)
-        
-        if self.gradient_checkpointing and self.training and use_cache:
-            warnings.warning_once("`use_cache=True` is incompatible with gradient checkpointing. Setting `use_cache=False`...")
-            use_cache = False
 
+        # Initialize the ret_stateList
+        ret_stateList = self.get_init_state(batch_size=x_hidden_state.shape[0], skip_init_state=True)
+       
         all_hidden_states = () if output_hidden_states else None
         all_attns = () if output_attentions else None
         v_first = None
         ret_sublist = None
         
-        # Lets start iterating
+        # Lets start iterating the blocks
         for i, block in enumerate(self.blocks):
             # Build the full inner hidden state
             if output_hidden_states:
@@ -283,3 +292,164 @@ class RWKV7Model(RWKV7PreTrainedModel):
             hidden_states=all_hidden_states,
             attentions=all_attns
         )
+
+@add_start_docstrings(
+    """
+    The RWKV Model transformer with a language modeling head on top (linear layer with weights tied to the input
+    embeddings).
+    """,
+    RWKV7_START_DOCSTRING,
+)
+class RWKV7ForCausalLM(RWKV7Model, GenerationMixin):
+
+    def __init__(self, config):
+        super().__init__(config)
+        self.post_init()
+
+    def prepare_inputs_for_generation(
+        self, 
+        input_ids=None, 
+        attention_mask: Optional[torch.Tensor] = None,
+        inputs_embeds: Optional[torch.Tensor] = None,
+        use_cache: bool = True,
+        rwkv_state: Optional[list[tuple[torch.Tensor,torch.Tensor,torch.Tensor]]] = None,
+        # num_new_tokens_if_rwkv_state: int = 1, # Only triggers if given input_ids + rwkv_state
+        num_logits_to_keep: Optional[int] = None,
+        **kwargs
+    ):
+        '''
+        Personal Notes: On huggingface barely documented "Transformer" hooks.
+
+        I assume this is triggered once, for the start of AI inference.
+        With subsequent calls for forward on each token step, being updated with
+        `_update_model_kwargs_for_generation` function instead?
+        '''
+        # # only last token for `inputs_ids` if the `past_key_values` is passed along.
+        # if rwkv_state is not None and input_ids is not None:
+        #     input_ids = input_ids[:, -num_new_tokens_if_rwkv_state:]
+        
+        # if `inputs_embeds` are passed, we only want to use them in the 1st generation step
+        if inputs_embeds is not None:
+            if input_ids is not None:
+                raise ValueError("You cannot specify both `inputs_ids` and `inputs_embeds` at the same time")
+            model_inputs = {'inputs_embeds': inputs_embeds}
+        else:
+            # The `contiguous()` here is necessary to have a static stride during decoding. torchdynamo otherwise
+            # recompiles graphs as the stride of the inputs is a guard.
+            # Ref: https://github.com/huggingface/transformers/pull/29114
+            # TODO: use `next_tokens` directly instead.
+            model_inputs = {'input_ids': input_ids.contiguous()}
+
+        if num_logits_to_keep is not None:
+            model_inputs['num_logits_to_keep'] = num_logits_to_keep
+
+        model_inputs.update({
+            'rwkv_state': rwkv_state,
+            'use_cache': use_cache,
+            'attention_mask': attention_mask,
+            'num_logits_to_keep': num_logits_to_keep,
+        })
+        return model_inputs
+
+    def _update_model_kwargs_for_generation(
+        self, outputs: ModelOutput,
+        model_kwargs: Dict[str, Any],
+        num_new_tokens: int = 1,
+        **kwargs
+    ) -> Dict[str, Any]:
+        # Overwritten -- this model uses `state`, but doesn't have a cache (`past_key_values`)
+        rwkv_state = outputs.get("rwkv_state", None)
+        input_ids = model_kwargs.get("input_ids", None)
+        attention_mask = model_kwargs.get("attention_mask", None)
+
+        # only last token for inputs_ids if the state is passed along.
+        if rwkv_state is not None and input_ids is not None and num_new_tokens > 0:
+            input_ids = input_ids[:, -num_new_tokens:]
+            model_kwargs["input_ids"] = input_ids
+
+            if attention_mask is not None:
+                attention_mask = attention_mask.new_ones((attention_mask.shape[0], num_new_tokens))
+                model_kwargs["attention_mask"] = attention_mask
+
+        # Return the formated output
+        return model_kwargs
+    
+    @add_start_docstrings_to_model_forward(RWKV7_INPUTS_DOCSTRING)
+    @add_code_sample_docstrings(
+        output_type=RWKV7CausalLMOutput,
+    )
+    def forward(
+        self,
+        input_ids: Optional[torch.LongTensor] = None,
+        attention_mask: Optional[torch.LongTensor] = None,  # noqa
+        inputs_embeds: Optional[torch.FloatTensor] = None,
+        labels: Optional[torch.LongTensor] = None,
+        rwkv_state: Optional[list[tuple[torch.Tensor,torch.Tensor,torch.Tensor]]] = None,
+        use_cache: Optional[bool] = None,
+        output_attentions: Optional[bool] = None,
+        output_hidden_states: Optional[bool] = None,
+        return_dict: Optional[bool] = None,
+        **kwargs
+    ) -> Union[Tuple, RWKV7CausalLMOutput]:
+        r"""
+        labels (`torch.LongTensor` of shape `(batch_size, sequence_length)`, *optional*):
+            Labels for language modeling. Note that the labels **are shifted** inside the model, i.e. you can set
+            `labels = input_ids` Indices are selected in `[-100, 0, ..., config.vocab_size]` All labels set to `-100`
+            are ignored (masked), the loss is only computed for labels in `[0, ..., config.vocab_size]`
+        """
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+        rwkv_outputs = RWKV7Model.forward(
+            self, input_ids, attention_mask, inputs_embeds, 
+            rwkv_state, use_cache, output_attentions, output_hidden_states,
+            return_dict=False
+        )
+
+        # Get the hidden state, and the updated RWKV state
+        hidden_states = rwkv_outputs[0]
+        rwkv_state = rwkv_outputs[1]
+
+        # Get the ALL hidden states and attentions dumps
+        all_hidden_states = rwkv_outputs[2] if output_hidden_states else None
+        if output_hidden_states:
+            all_attns = rwkv_outputs[3] if output_attentions else None
+        else:
+            all_attns = rwkv_outputs[2] if output_attentions else None
+        
+        # Forward the head state
+        logits = self.head(hidden_states)
+
+        # Compute the loss from the labels
+        loss = None
+        if labels is not None:
+
+            # Setup loss function
+            if self._loss_function_cache is None:
+                self._loss_function_cache = CrossEntropyLoss()
+
+            # move labels to correct device to enable model parallelism
+            labels = labels.to(logits.device)
+            # Shift so that tokens < n predict n
+            shift_logits = logits[..., :-1, :].contiguous()
+            shift_labels = labels[..., 1:].contiguous()
+            # Compute the token loss
+
+            if attention_mask is not None:
+                token_loss = F.cross_entropy(shift_logits.view(-1, shift_logits.size(-1)), shift_labels.view(-1), reduction="none")
+                submask = attention_mask[..., 1:].contiguous().view(-1)
+                loss = (token_loss * submask).sum() / submask.sum()
+            else:
+                loss = F.cross_entropy(shift_logits.view(-1, shift_labels.size(-1)), shift_labels.view(-1), reduction="mean")
+
+        if not return_dict:
+            return tuple(i for i in [loss, logits, rwkv_state, all_hidden_states, all_attns] if i is not None)
+        
+        return RWKV7CausalLMOutput(
+            loss=loss,
+            logits=logits,
+            rwkv_state=rwkv_state,
+            hidden_states=all_hidden_states,
+            attentions=all_attns,
+        )
+
+__all__ = ["RWKV7ForCausalLM", "RWKV7Model", "RWKV7PreTrainedModel"]
